bfda1dd2e11bb3dc858879741690dee731d57ff7
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-12-28 22:40:22 -0500

    Create a Class and test for the question

--- src/main/java/Arrays/TwoNumberSum.java
+++ src/main/java/Arrays/TwoNumberSum.java
@@ -1,4 +1,25 @@
 package Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoNumberSum {
+    private int[] array;
+    private int targetSum;
+    public TwoNumberSum(int[] array, int targetSum){
+        this.array = array;
+        this.targetSum = targetSum;
+    }
+
+    public int[] twoNumberSum(int[] array, int targetSum) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : array)
+            if(set.contains(targetSum - num))
+                return new int[]{num, targetSum - num};
+            else
+                set.add(num);
+
+        return new int[0];
+    }
 }
---
+++ src/test/java/Arrays/TwoNumberSumTest.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoNumberSumTest {
+
+    @Test
+    void twoNumberSum() {
+    }
+}
