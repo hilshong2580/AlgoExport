4841f0a55660da5488c53aefe4acae8ca0eec383
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2022-05-02 23:23:52 -0400

    Strings Question: Write a function that takes in a string of lowercase English-alphabet letters and returns the index of the string's first non-repeating character. (Not finish yet)

---
+++ src/main/java/Strings_FirstNonRepeatingCharacter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/*
+
+Write a function that takes in a string of lowercase English-alphabet letters and returns the index
+of the string's first non-repeating character.
+
+The first non-repeating character is the first character in a string that occurs only once.
+If the input string doesn't have any non-repeating characters, your function should return -1.
+
+Sample
+string = "abcdcaf"
+1
+
+*/
+public class Strings_FirstNonRepeatingCharacter {
+    public int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        return -1;
+    }
+}
