c4b7744596ad9ae44816a17bab428d4af8c31015
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2022-06-04 19:49:53 -0400

    Idea: Have to sort the intervals by "Arrays.sort(sortedIntervals, (a, b) -> Integer.compare(a[0], b[0]));" If overlapping, store the min and max value from two set value.

--- src/main/java/Arrays_MergeOverlappingIntervals.java
+++ src/main/java/Arrays_MergeOverlappingIntervals.java
@@ -1,32 +1,67 @@
 import java.util.*;
 
+
+/*
+Write a function that takes in a non-empty array of arbitrary intervals, merges any overlapping
+intervals, and returns the new intervals in no particular order.
+
+Each interval is an array of two integers, with interval[0] as the start of the interval and
+interval[1] as the end of the interval.
+
+Note that back-to-back intervals aren't considered to be overlapping. For example, [1,5] and [6, 7]
+aren't overlapping; however, [1, 6] and [, 7] are indeed overlapping.
+
+Also note that the start of any particular interval will always be less than or equal to the end of
+that interval.
+
+input :[
+  [1, 2],
+  [3, 5],
+  [4, 7],
+  [6, 8],
+  [9, 10]
+]
+
+
+Idea:
+Have to sort the intervals by "Arrays.sort(sortedIntervals, (a, b) -> Integer.compare(a[0], b[0]));"
+If overlapping, store the min and max value from two set value.
+
+
+*/
 public class Arrays_MergeOverlappingIntervals {
-
     public int[][] mergeOverlappingIntervals(int[][] intervals) {
-        TreeMap<Integer, Integer> treeMap = new TreeMap<Integer, Integer>();
-        int loop = 0, h = 0, k = 1;
+        List<Integer[]> aList = new ArrayList<>();
+        int index = 0;
+        int[][] sortedIntervals = intervals.clone();
+        Arrays.sort(sortedIntervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        for (int[] i : intervals)
-            treeMap.put(i[0], i[1]);
+        int overFir = sortedIntervals[0][0], overSec = sortedIntervals[0][1];
 
-        while (loop != intervals.length - 1) {
-            if (treeMap.get(intervals[h][0]) >= intervals[k][0]) {
-                treeMap.put(intervals[h][0], treeMap.get(intervals[k][0]));
-                treeMap.remove(intervals[k][0]);
-                k++;
-            } else {
-                h = k;
-                k++;
+        for(int i = 1; i < sortedIntervals.length; i++)
+        {
+
+            if( overSec >= sortedIntervals[i][0]){
+                overFir = Math.min(overFir, sortedIntervals[i][0]);
+                overSec = Math.max(overSec, sortedIntervals[i][1]);
             }
-            loop++;
-        }
+            else {
+                aList.add(new Integer[]{overFir, overSec});
+                index++;
+                overFir = sortedIntervals[i][0];
+                overSec = sortedIntervals[i][1];
+            }
 
-        int[][] result = new int[treeMap.size()][2];
-        loop = 0;
-        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
-            result[loop][0] = entry.getKey();
-            result[loop][1] = entry.getValue();
-            loop++;
+        }
+        aList.add(new Integer[]{overFir, overSec});
+
+        int[][] result = new int[aList.size()][2];
+        index = 0;
+
+        for(Integer[] x : aList){
+            result[index][0] = x[0];
+            result[index][1] = x[1];
+            index++;
         }
 
         return result;
